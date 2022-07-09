55eb03b4f7848f0d2442b5fd9c55ce118e10ffbc
chyod-chen <50770840+chyod-chen@users.noreply.github.com>
2022-07-09 19:43:37 +0800

    Delete src directory

--- src/utils/hasPermission.js
+++
@@ -1,7 +0,0 @@
-import store from '../store'
-
-export function hasPermission(permission) {
-  permission=permission.trim()
-  let myPermissions = store.getters.permissions;
-  return myPermissions.indexOf(permission) > -1;
-}
