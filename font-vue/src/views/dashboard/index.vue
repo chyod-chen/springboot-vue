3ef6b4c87a6242794d1a94fc8921763239926a7b
chyod-chen <50770840+chyod-chen@users.noreply.github.com>
2022-07-09 19:58:35 +0800

    Add files via upload

---
+++ font-vue/src/views/dashboard/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="dashboard-container">
+    <el-carousel :interval="3000" type="card" height="500px">
+      <el-carousel-item>
+        <img :src="student" alt="1">
+      </el-carousel-item>
+      <el-carousel-item>
+        <img :src="teacher" alt="2">
+      </el-carousel-item>
+      <el-carousel-item>
+        <img :src="classes" alt="3">
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+</template>
+<script>
+import student from '@/assets/student.png'
+import teacher from '@/assets/teacher.png'
+import classes from '@/assets/classes.png'
+  export default {
+    name: 'dashboard',
+    data() {
+      return {
+        student,teacher,classes
+      }
+    },
+  }
+</script>
+<style>
+  .dashboard-container {
+    margin-top: 5%;
+  }
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 200px;
+    margin: 0;
+  }
+  
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+  
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
+</style>
